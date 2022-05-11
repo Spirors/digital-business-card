4e988493fc6c443afb3654ca724f09c7eea21fb4
siyuan zou <siyuanzou99@gmail.com>
2022-05-11 14:24:24 -0400

    the skeleton website is working

---
+++ src/components/Info.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import FaceImg from '../images/face.jpg'
+
+export default function Interests() {
+    return (
+        <div>
+            <img src={FaceImg} alt="This is my face" />
+        </div>
+    )
+}
