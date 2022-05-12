8bbc1bbf26322a12adcb26c20a3b0bcca04dca7e
siyuan zou <siyuanzou99@gmail.com>
2022-05-12 12:16:25 -0400

    about component done

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function About() {
     return (
-        <div>
-            <h3>About</h3>
-            <p>Thing about me</p>
+        <div className='about--container'>
+            <h3 className='about--title'>About</h3>
+            <p className='about--text'>I am currently a software developer working at Fiserv's Professional Service team. We deal with implementing Architect solution for different Financial Insitutions. I graduated from Stony Brook University May of 2020 with a bachelor of science in computer science.</p>
         </div>
     )
 }
