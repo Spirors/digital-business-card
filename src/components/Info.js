c647132da956f59d20a0eb7198746cead6dcab2a
siyuan zou <siyuanzou99@gmail.com>
2022-05-12 11:43:56 -0400

    info component done

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import FaceImg from '../images/face.jpg'
+import FaceImg from '../images/Face.jpg'
 
 export default function Interests() {
     return (
-        <div>
-            <img src={FaceImg} alt="This is my face" />
+        <div className='info--container'>
+            <img className='info--face' src={FaceImg} alt="This is my face" />
+            <h2 className='info--name'>Siyuan Zou</h2>
+            <h4 className='info--role'>Fiserv Developer</h4>
+            <p className='info--website'>
+                <a href='/'>siyuanzou.website</a>
+            </p>
+            <div className='info--btn--container'>
+                <button className='info--emailbtn'><i class="bi bi-envelope-fill"></i> Email</button>
+                <button className='info--linkedinbtn'><i class="bi bi-linkedin"></i> LinkedIn</button>
+            </div>
         </div>
     )
 }
